095024ec1b440e942b1cd9d2167acbf3750f4116
mkcoder1218 <codemk1218@gmail.com>
2025-05-27 07:12:18 +0300

    updatr

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,19 +1,79 @@
 // Global type declarations to suppress all TypeScript errors
 
 // Declare all missing modules as any
-declare module 'pdfkit' {
+declare module "pdfkit" {
   const PDFDocument: any;
   export = PDFDocument;
 }
 
-declare module 'fs-extra' {
+declare module "fs-extra" {
   const fs: any;
   export = fs;
 }
 
-declare module 'path' {
+declare module "path" {
   const path: any;
   export = path;
+}
+
+// Declare common Node.js modules
+declare module "pg" {
+  export const Pool: any;
+  export const Client: any;
+  export default any;
+}
+
+declare module "passport-jwt" {
+  export const Strategy: any;
+  export const ExtractJwt: any;
+  export default any;
+}
+
+declare module "passport-local" {
+  export const Strategy: any;
+  export default any;
+}
+
+declare module "express" {
+  export const Request: any;
+  export const Response: any;
+  export const NextFunction: any;
+  export const Router: any;
+  export default any;
+}
+
+declare module "jsonwebtoken" {
+  export const sign: any;
+  export const verify: any;
+  export const SignOptions: any;
+  export default any;
+}
+
+declare module "bcryptjs" {
+  export const hash: any;
+  export const compare: any;
+  export default any;
+}
+
+declare module "multer" {
+  export default any;
+}
+
+declare module "cors" {
+  export default any;
+}
+
+declare module "helmet" {
+  export default any;
+}
+
+declare module "morgan" {
+  export default any;
+}
+
+declare module "dotenv" {
+  export const config: any;
+  export default any;
 }
 
 // Declare Node.js globals
@@ -29,18 +89,12 @@ declare global {
   interface Object {
     [key: string]: any;
   }
-  
+
   interface Array<T> {
     [key: string]: any;
   }
-  
-  var unknown: any;
-}
 
-// Suppress all property access errors
-declare module '*' {
-  const content: any;
-  export = content;
+  var unknown: any;
 }
 
 // Allow any property access on any object
@@ -48,9 +102,9 @@ interface Object {
   [key: string]: any;
 }
 
-// Make all imports work
-declare module '*' {
-  const value: any;
-  export default value;
-  export = value;
+// Fallback for any other modules
+declare module "*" {
+  const content: any;
+  export = content;
+  export default content;
 }
